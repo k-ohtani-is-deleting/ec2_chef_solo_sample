747a2b2618b37fd88f9448191da44594f2dd139d
Ryutaro YOSHIBA <ryuzee@gmail.com>
2012-01-14 08:45:59 +0900

    update for article

--- amzn-bootstrap.sh
+++ amzn-bootstrap.sh
@@ -3,7 +3,7 @@ set -e -x
 LOGFILE=/tmp/log.txt
 echo "==Start!==" > $LOGFILE
 yum upgrade -y >> $LOGFILE 2>&1
-yum install -y gcc make ruby-devel rubygems git python-setuptools >> $LOGFILE 2>&1
+yum install -y gcc make ruby-devel rubygems python-setuptools >> $LOGFILE 2>&1
 gem install chef --no-ri --no-rdoc >> $LOGFILE 2>&1
 mkdir /etc/chef /var/chef >> $LOGFILE 2>&1
 chmod -R 666 /etc/chef /var/chef >> $LOGFILE 2>&1
