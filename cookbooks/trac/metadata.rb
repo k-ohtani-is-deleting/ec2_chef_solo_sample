c54db146218c4e7ef3701cfeb36e99a182da1e24
Ryutaro YOSHIBA <ryuzee@gmail.com>
2012-01-14 08:47:24 +0900

    delete trac

--- cookbooks/trac/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Ryuzee"
-maintainer_email "ryuzee@gmail.com"
-license          "MIT License"
-description      "Install Trac and Subversion"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
